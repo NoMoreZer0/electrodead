c5b46870ed4c9d8b9513473c449e5faed3e0d565
NoMoreZer0 <kazakupersuper@mail.ru>
2022-04-28 17:31:23 +0600

    server of the website

--- server.js
+++ server.js
@@ -4,6 +4,41 @@ const {request, response} = require("express");
 const app = express();
 const port = 3000;
 
+//setting css and js
+app.use(express.static('Images'));
+app.use(express.static('audio'));
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('fontawesome-free-6.0.0-beta3-web'));
+
+//initiate view engine
+app.set('view engine', 'ejs');
+app.set('views', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/computer', (req, res) => {
+    res.render('computer');
+});
+
+app.get('/devices', (req, res) => {
+   res.render('devices');
+});
+
+app.get('/feedback', (req, res) => {
+   res.render('feedback');
+});
+
+app.get('/mobile-devices', (req, res) => {
+   res.render('mobile-devices');
+});
+
+app.get('/signin-signup', (req, res) => {
+    res.render('signin-signup');
+});
+
 const axios = require('axios');
 const cors = require('cors');
 app.use(cors());
